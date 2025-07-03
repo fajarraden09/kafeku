43bd26146b897c4e34ef654c2eb4fed9f213e22c
fajarraden09 <fajarraden9019@gmail.com>
2025-07-04 00:50:22 +0700

    terahkir

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -10,7 +10,7 @@
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="{{ asset('Lte/plugins/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="{{ asset(';Lte/dist/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('Lte/dist/css/adminlte.min.css') }}">
 </head>
 
 <body class="hold-transition sidebar-mini">
