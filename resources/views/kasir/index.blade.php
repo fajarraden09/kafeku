0d5371beae19a2cda61c27197f58524f95d2b6e3
fajarraden09 <fajarraden9019@gmail.com>
2025-07-16 18:59:19 +0700

    adit tambah

--- resources/views/kasir/index.blade.php
+++ resources/views/kasir/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="content-wrapper">
-        <section class="content-header">
+        {{-- <section class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
@@ -10,7 +10,7 @@
                     </div>
                 </div>
             </div>
-        </section>
+        </section> --}}
 
         <section class="content">
             <div class="container-fluid">
