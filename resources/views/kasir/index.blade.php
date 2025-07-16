084d30692f70790f702e891fb8e8def9ccb4b74e
fajarraden09 <fajarraden9019@gmail.com>
2025-07-16 19:00:27 +0700

    edit lage

--- resources/views/kasir/index.blade.php
+++ resources/views/kasir/index.blade.php
@@ -2,15 +2,12 @@
 
 @section('content')
     <div class="content-wrapper">
-        {{-- <section class="content-header">
+        <section class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0">Menu Kasir</h1>
-                    </div>
                 </div>
             </div>
-        </section> --}}
+        </section>
 
         <section class="content">
             <div class="container-fluid">
