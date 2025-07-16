7ef0a12c01c4619d5d62649ffbe9a6f400c04dbb
fajarraden09 <fajarraden9019@gmail.com>
2025-07-16 19:06:35 +0700

    perbaikan

--- resources/views/kasir/index.blade.php
+++ resources/views/kasir/index.blade.php
@@ -3,8 +3,6 @@
 @section('content')
     <div class="content-wrapper">
         <section class="content-header">
-            <div class="container-fluid">
-            </div>
         </section>
 
         <section class="content">
