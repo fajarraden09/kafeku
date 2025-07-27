1260ae0910527b8df7ace23815d94520b835ac50
fajarraden09 <fajarraden9019@gmail.com>
2025-07-28 01:17:41 +0700

    ayo bisa

--- resources/views/laporan/stok.blade.php
+++ resources/views/laporan/stok.blade.php
@@ -47,37 +47,42 @@
 
         <section class="content">
             <div class="container-fluid">
+                {{-- Kumpulan Tombol Filter dan Pencarian --}}
+                <div id="filter-controls" class="mb-3 d-flex align-items-center">
+                    <a href="{{ route('owner.laporan.stok') }}" class="btn btn-secondary mr-2">Tampilkan Semua</a>
+                    <a href="{{ route('owner.laporan.stok', ['filter' => 'hari_ini']) }}" class="btn btn-info mr-2">Hari
+                        Ini</a>
+                    <a href="{{ route('owner.laporan.stok', ['filter' => 'minggu_ini']) }}" class="btn btn-info mr-2">Minggu
+                        Ini</a>
+                    <a href="{{ route('owner.laporan.stok', ['filter' => 'bulan_ini']) }}" class="btn btn-info">Bulan
+                        Ini</a>
+
+                    <div class="ml-auto d-flex align-items-center">
+                        <form action="{{ route('owner.laporan.stok') }}" method="GET" class="d-flex">
+                            <input type="date" class="form-control" name="tanggal" value="{{ $tanggal_pencarian ?? '' }}">
+                            <button class="btn btn-primary ml-2" type="submit">
+                                <i class="fas fa-search"></i>
+                            </button>
+                        </form>
+
+                        @if (isset($tanggal_pencarian))
+                            <button id="btnPrint" class="btn btn-success ml-2">
+                                <i class="fas fa-print"></i> Cetak Laporan
+                            </button>
+                        @else
+                            {{-- Tombol Cetak Umum --}}
+                            <button id="btnPrint" class="btn btn-primary ml-2"><i class="fas fa-print"></i> Cetak
+                                Laporan</button>
+                        @endif
+                    </div>
+                </div>
+
+                {{-- ======================================================= --}}
+                {{-- BAGIAN YANG DIUBAH UNTUK MEMBUAT TABEL BERDAMPINGAN --}}
+                {{-- ======================================================= --}}
                 <div class="row">
-                    <div class="col-12">
-
-                        {{-- Kumpulan Tombol Filter dan Pencarian --}}
-                        <div id="filter-controls" class="mb-3 d-flex align-items-center">
-                            <a href="{{ route('owner.laporan.stok') }}" class="btn btn-secondary mr-2">Tampilkan Semua</a>
-                            <a href="{{ route('owner.laporan.stok', ['filter' => 'hari_ini']) }}"
-                                class="btn btn-info mr-2">Hari Ini</a>
-                            <a href="{{ route('owner.laporan.stok', ['filter' => 'minggu_ini']) }}"
-                                class="btn btn-info mr-2">Minggu Ini</a>
-                            <a href="{{ route('owner.laporan.stok', ['filter' => 'bulan_ini']) }}"
-                                class="btn btn-info">Bulan Ini</a>
-
-                            <div class="ml-auto d-flex align-items-center">
-                                <form action="{{ route('owner.laporan.stok') }}" method="GET" class="d-flex">
-                                    <input type="date" class="form-control" name="tanggal"
-                                        value="{{ $tanggal_pencarian ?? '' }}">
-                                    <button class="btn btn-primary ml-2" type="submit">
-                                        <i class="fas fa-search"></i>
-                                    </button>
-                                </form>
-
-                                @if (isset($tanggal_pencarian))
-                                    <button id="btnPrint" class="btn btn-success ml-2">
-                                        <i class="fas fa-print"></i> Cetak Laporan
-                                    </button>
-                                @endif
-                            </div>
-                        </div>
-
-                        {{-- Tabel Riwayat Masuk --}}
+                    {{-- Kolom Kiri untuk Riwayat Masuk --}}
+                    <div class="col-lg-6">
                         <div class="card">
                             <div class="card-header">
                                 <h3 class="card-title">Riwayat Bahan Baku Masuk</h3>
@@ -109,9 +114,11 @@
                                 </table>
                             </div>
                         </div>
+                    </div>
 
-                        {{-- Tabel Riwayat Keluar --}}
-                        <div class="card mt-4">
+                    {{-- Kolom Kanan untuk Riwayat Keluar --}}
+                    <div class="col-lg-6">
+                        <div class="card">
                             <div class="card-header">
                                 <h3 class="card-title">Riwayat Bahan Baku Keluar</h3>
                             </div>
@@ -139,15 +146,21 @@
                                             <tr>
                                                 <td colspan="5" class="text-center">Tidak ada data.</td>
                                             </tr>
-                                            {{-- INI BAGIAN YANG DIPERBAIKI --}}
                                         @endforelse
                                     </tbody>
                                 </table>
                             </div>
                         </div>
+                    </div>
+                </div>
+                {{-- ======================================================= --}}
+                {{-- AKHIR DARI BAGIAN YANG DIUBAH --}}
+                {{-- ======================================================= --}}
 
-                        {{-- Tabel Stok Saat Ini --}}
-                        <div class="card mt-4">
+                {{-- Tabel Stok Saat Ini (Tetap di bawah dengan lebar penuh) --}}
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card mt-2">
                             <div class="card-header">
                                 <h3 class="card-title">Ringkasan Stok Bahan Baku Saat Ini</h3>
                             </div>
